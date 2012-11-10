077b8a5e00c9925d65d0940b2578754aabc044bf
Adam England <adam.n.england@gmail.com>
2012-11-10 17:46:02 -0600

    Error handling for twitter shares

--- app/controllers/Offers.java
+++ app/controllers/Offers.java
@@ -4,6 +4,7 @@ package controllers;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.net.URLEncoder;
 import java.util.Date;
 import java.util.List;
@@ -14,10 +15,12 @@ import models.User;
 import play.libs.WS;
 import play.libs.WS.HttpResponse;
 import play.mvc.Controller;
+import play.mvc.Http.StatusCode;
 import siena.Model;
 import siena.Query;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 
 import dwolla.DwollaTransfer;
 
@@ -41,11 +44,25 @@ public class Offers extends Controller {
 	}
 	
 	public static void accept(Long id) throws Exception {
+		System.out.println("Accept offer");
 		String token = request.headers.get("authorization").values.get(0);
 		User user = all().filter("token", token.replaceAll("\"", "")).get();
 		Offer offer = Model.all(Offer.class).filter("id", id).get();
 		String url = "https://api.singly.com/types/statuses?access_token="+ user.singlyAccessToken + "&to="+offer.type.toLowerCase()+"&body="+ URLEncoder.encode(offer.content,"UTF-8");
 		HttpResponse post = WS.url(url).post();
+		System.out.println(post.getStatus());
+		System.out.println(post.getString());
+		JsonElement twitter = post.getJson().getAsJsonObject().get("twitter");
+		System.out.println("Error?");
+		System.out.println(twitter.getAsJsonObject().get("errors"));
+		System.out.println(twitter.getAsJsonObject().get("errors") != null);
+		if (twitter.getAsJsonObject().get("errors") != null) {
+			response.status = StatusCode.BAD_REQUEST;
+			renderJSON(twitter.getAsJsonObject().get("errors").toString());
+		} else {
+			BigInteger asBigInteger = twitter.getAsJsonObject().get("id").getAsBigInteger();
+			System.out.println(asBigInteger);			
+		}
 		Acceptance acceptance = new Acceptance();
 		acceptance.acceptor = user;
 		acceptance.offer = offer;
